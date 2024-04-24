5025123aeed32d0d9744b0aa0488b75fabb04398
BillStephens2022 <two4onebill@yahoo.com>
2024-04-24 15:41:24 -0400

    added a folder for the countdown problem and added notes describing the problem

---
+++ CountdownProblem/countdown.hs
@@ -0,0 +1,10 @@
+-- The Countdown Problem --
+
+-- Example
+-- Using the numbers:
+--    1 3 7 10 25 50
+-- and the arithmetic operators:
+--     + - * /
+-- construct an expression whose value is: 765
+
+
