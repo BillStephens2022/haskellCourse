95529953010767b80e8ccf5a16cc686cfad2bd25
BillStephens2022 <two4onebill@yahoo.com>
2024-04-26 13:37:06 -0400

    updated with more efficient solution to sudoku puzzles

--- Sudoku/sudoku.hs
+++ Sudoku/sudoku.hs
@@ -23,6 +23,10 @@ single :: [a] -> Bool
 single [_] = True
 single _  =  False
 
+-- Empty Grid
+blank :: Grid
+blank = replicate 9 (replicate 9 '.')
+
 -- Easy Grid - note the dots represent blank cells in the grid
 easy :: Grid
 easy = ["2....1.38",
@@ -35,9 +39,42 @@ easy = ["2....1.38",
         ".5..69784",                           
         "4..25...."]
 
--- Empty Grid
-blank :: Grid
-blank = replicate 9 (replicate 9 '.')
+-- Gentle Grid - slightly higher difficulty than Easy
+gentle :: Grid
+gentle = [".1.42...5",
+         "..2.71.39",
+         ".......4.",
+         "2.71....6",
+         "....4....",
+         "6....74.3",
+         ".7.......",
+         "12.73.5..",
+         "3...82.7."]
+
+-- diabolical Grid (more difficulty)
+diabolical :: Grid
+diabolical =  [".9.7..86.",
+              ".31..5.2.",
+              "8.6......",
+              "..7.5...6",
+              "...3.7...",
+              "5...1.7..",
+              "......1.9",
+              ".2.6..35.",
+              ".54..8.7."]
+
+-- "unsolvable" Grid -  to be used for testing:
+-- note the 1 in first column of row 1 and 2
+unsolvable :: Grid
+unsolvable = ["1..9.7..3",
+             "18.....7.",
+             "..9...6..",
+             "..72.94..",
+             "41.....95",
+             "..85.43..",
+             "..3...7..",
+             ".5.....4.",
+             "2..8.6..9"]
 
 -- define rows
 rows :: Matrix a -> [Row a]
@@ -171,3 +208,81 @@ fix f x = if x == x' then x else fix f x'
 
 -- Back to the drawing board...
 
+-- Properties of matrices
+--------------------------
+
+-- In this section we introduce a number of properties that may hold of
+-- a matrix of choices.  First of all, let us say that such a matrix is
+-- "complete" if each square contains a single choice:
+
+complete :: Matrix Choices -> Bool
+complete =  all (all single)
+
+-- Similarly, a matrix is "void" if some square contains no choices:
+
+void :: Matrix Choices -> Bool
+void =  any (any null)
+
+-- In turn, we use the term "safe" for matrix for which all rows,
+-- columns and boxes are consistent, in the sense that they do not
+-- contain more than one occurrence of the same single choice:
+
+safe :: Matrix Choices -> Bool
+safe cm = all consistent (rows cm) &&
+          all consistent (cols cm) &&
+          all consistent (boxs cm)
+
+consistent :: Row Choices -> Bool
+consistent =  nodups . concat . filter single
+
+-- Finally, a matrix is "blocked" if it is void or unsafe:
+
+blocked :: Matrix Choices -> Bool
+blocked m = void m || not (safe m)
+
+
+-- Making choices one at a time
+--------------------------------
+
+-- Clearly, a blocked matrix cannot lead to a solution.  However, our
+-- previous solver does not take account of this.  More importantly,
+-- a choice that leads to a blocked matrix can be duplicated many
+-- times by the collapse function, because this function simply
+-- considers all possible combinations of choices.  This is the 
+-- primary source of inefficiency in our previous solver.
+
+-- This problem can be addressed by expanding choices one square at
+-- a time, and filtering out any resulting matrices that are blocked
+-- before considering any further choices.  Implementing this idea
+-- is straightforward, and gives our final Sudoku solver:
+
+solve4 :: Grid -> [Grid]
+solve4 = search . prune . choices
+
+search                :: Matrix Choices -> [Grid]
+search m
+    | blocked m = []
+    | complete m = collapse m
+    | otherwise = [g | m' <- expand m, 
+                   g  <- search (prune m')]
+
+-- The function expand behaves in the same way as collapse, except that
+-- it only collapses the first square with more than one choice:
+
+expand :: Matrix Choices -> [Matrix Choices]
+expand m = 
+    [rows1 ++ [row1 ++ [c] : row2] ++ rows2 | c <- cs]
+        where
+            (rows1,row:rows2) = break (any (not . single)) m
+            (row1,cs:row2)    = break (not . single) row
+
+-- Note that there is no longer any need to check for valid grids at 
+-- the end, because the process by which solutions are constructed 
+-- guarantees that this will always be the case.  There also doesn't
+-- seem to be any benefit in using "fix prune" rather than "prune"
+-- above; the program is faster without using fix.  In fact, our
+-- program now solves any newspaper Sudoku puzzle in an instant!
+
+-- Exercise: modify the expand function to collapse a square with the
+-- smallest number of choices greater than one, and see what effect 
+-- this change has on the performance of the solver.
