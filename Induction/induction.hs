fa7ceea1660f927dbdc5578476292602ac783dc9
BillStephens2022 <two4onebill@yahoo.com>
2024-04-30 07:26:39 -0400

    created a quickReverse function using induction to improve the efficiency of the standard reverse function

---
+++ Induction/induction.hs
@@ -0,0 +1,70 @@
+--(++) :: [a] -> [a] -> [a]
+--[] ++ ys = ys
+--(x:xs) ++ ys = x : (xs ++ ys)
+
+--reverse :: [a] -> [a]
+--reverse [] = []
+--reverse (x:xs) = reverse xs ++ [x]
+
+-- how many evaluation steps does xs ++ ys take?
+-- Example: [1,2] ++ [3]
+-- = 1 : ([2] ++ [3])
+-- = 1 : (2 : ([] ++ [3]))
+-- = 1 : 2 : [3]
+
+-- this took 3 steps to evaluate
+-- in conclusion, xs ++ ys takes (length xs + 1) steps
+-- this takes linear time to evaluate, which is not that efficient
+
+-- How many steps for the reverse function?
+-- Example:  reverse [1,2]
+-- = reverse [2] ++ [1]
+-- = (reverse [] ++ [2]) ++ [1]
+-- = ([] ++ [2]) ++ [1]
+-- this took 2 steps to evaluate the reverse function
+-- now we need to add steps for evaluating the append ('++') function
+-- from above we know that append takes length xs + 1 steps, which will be 3 steps
+-- so, in general, reverse xs takes length 1 + 2 + ... +  (n + 1) where n = length xs 
+-- using Gauss' formula to calculate a sum of a series of numbers from 1 to n = (n+1)(n+2) / 2 = (n^2 + 3n +2) /2
+-- so this reverse function takes quadratic time to solve, which is very inefficient
+
+-- so, first let's derive a new function for reverse' which combines reversing xs and appending ys to it. So,
+-- we know that the new function will have to have the same result as the one on the right below
+-- reverse' xs ys = (reverse xs) ++ ys
+-- base case: reverse' [] ys
+-- = (reverse []) ++ ys
+-- = [] ++ ys
+-- = ys
+-- inductive case: reverse' (x:xs) ys
+-- = reverse (x:xs) ++ ys
+-- = reverse (x:xs) ++ ys
+-- = (reverse xs ++ [x]) ++ ys
+-- = reverse xs ++ ([x] ++ ys)
+-- = reverse xs ++ (x:ys)
+-- = reverse' xs (x:ys)
+-- therefore my recursive definition will be:  reverse' (x:xs) ys = reverse' xs (x:ys)
+
+-- So now our final definition for reverse' will be:
+reverse' :: [a] -> [a] -> [a]
+reverse' [] ys = ys
+reverse' (x:xs) ys = reverse' xs (x:ys)
+-- note that in the final definition we eliminated "++", which we know is inefficient.
+
+-- using this, we can now change our reverse function so that it doesn't include ++
+quickReverse:: [a] -> [a]
+quickReverse xs = reverse' xs []
+
+-- Example quickReverse [1,2,3]
+-- = reverse' [1,2,3] []
+-- = reverse' [2,3] [1]
+-- = reverse' [3] [2,1]
+-- = reverse' [] [3,2,1]  -- now, we've reached the base case
+-- = [3,2,1]
+
+
+
+
+
+
+
+
---
+++ reasoning.md
@@ -0,0 +1,25 @@
+# Equational Reasoning
+
+## Properties of algebraic functions
+
+1. **Commutativity**:
+   - For addition: x + y = y + x
+   - For multiplication: x * y = y * x
+
+2. **Associativity**:
+   - For addition: x + (y + z) = (x + y) + z
+   - For multiplication: x * (y * z) = (x * y) * z
+
+3. **Distributivity**:
+   - Distributive property of multiplication over addition: 
+        x * (y + z) = (x * y) + (x * z)
+   - Distributive property of addition over multiplication:
+        (x + y) * z = (x * z) + (y * z)
+
+Example:
+(x + a) (x + b)
+= (x + a) x + (x + a) b
+= x^2 + ax + xb + ab
+= x^2 + ax + bx + ab
+= x^2 + (a + b)x + ab
+
