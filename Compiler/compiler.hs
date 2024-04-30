c330d310356e0063b618c4f47b4a9a04fe1e3409
BillStephens2022 <two4onebill@yahoo.com>
2024-04-30 11:51:31 -0400

    reformulated compiler function with proofs

--- Compiler/compiler.hs
+++ Compiler/compiler.hs
@@ -35,5 +35,55 @@ comp (Add x y) = comp x ++ comp y ++ [ADD]
 -- exec (comp e) [] = [eval e]
 --                  = (eval e):s
 
+-- Let's check if this compiler correctness equation is actually true
+-- Base case: e = Val n
+-- exec (comp (Val n)) s
+-- = exec [PUSH n] s
+-- = n:s
+-- = eval (Val n):s
+-- Inductive case: e = Add x y
+-- exec (comp (Add x y)) s
+-- = exec (comp x ++ comp y ++ [ADD]) s
+-- = exec (comp x ++ (comp y ++ [ADD])) s
+-- now we can apply the distributivity lemma
+-- = exec (comp y ++ [ADD]) (exec (comp x) s)
+-- = exec (comp y ++ [ADD]) ((eval x):s)
+-- = exec [ADD] (exec (comp y) ((eval x):s))
+-- = exec [ADD] (eval y : eval x : s)
+-- = (eval x + eval y) : s
+-- = eval (Add x y) : s
+-- Done!
+
+-- Let's re write our comp function
+quickComp :: Expr -> Code
+quickComp e = comp' []
+
+-- helper function comp'
+comp' :: Expr -> Code -> Code
+comp' (Val n) c = PUSH n:c
+comp' (Add x y) c = comp' x (comp' y (ADD:c))
+
+-- Compiler Correctness w/newly defined function
+-- exec (comp' e c) s = exec c (eval e : s)
+-- Base case: exec (comp' (Val n) c) s
+-- = exec (PUSH n : c) s
+-- = exec c (n : s)
+-- = exec c (eval (Val n) : s)
+-- Inductive case: exec (comp' (Add x y) c) s
+-- = exec (comp' x (comp' y (ADD : c))) s
+-- = exec (comp' y (ADD : c)) (eval x : s)
+-- = exec (ADD : c) (eval y : eval x : s)
+-- = exec c ((eval x + eval y) : s)
+-- = exec c (eval (Add x y) : s)
+
+-- Benefits from reformulating the compiler and compiler correctness theorem
+-- 1) 22 steps initially in proof reduced to 8 steps
+-- 2) original proof required 2 lemmas which was reduced to zero lemmas
+-- 3) reduced use of append (i.e. '++')...more efficient
+-- 4) original proof had a stack underflow issue, this reformulation does not
+
+
+
+
 
 
